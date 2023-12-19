1f63de69d7f1d8a8aabf6ffaa87e28e1bee8f4e5
was-siri-us <116163817+was-siri-us@users.noreply.github.com>
2023-12-19 10:36:49 +0530

    Update auth_routes.js

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -10,7 +10,7 @@ const {test, registerUser,loginUser} = require('../controllers/authController')
 router.use(
     cors({
         credentials: true,
-        origin: 'http://localhost:5173'
+        origin: 'https://dynamic-belekoy-623f12.netlify.app'
     })
 )
 
